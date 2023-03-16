9e20b5d7276671f3e3965f3ab463e88a087f10f0
Snowiiii <meieralexander65@gmail.com>
2023-03-16 18:33:25 +0100

    Structure: Refactoring

--- vent-common/src/component/input_component.rs
+++
@@ -1,7 +0,0 @@
-use winit::event::{ElementState, VirtualKeyCode};
-
-pub trait InputComponent {
-    fn process_keyboard(&mut self, key: VirtualKeyCode, state: ElementState);
-
-    fn process_mouse(&mut self, mouse_dx: f64, mouse_dy: f64);
-}
--- vent-common/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod component;
-pub mod entity;
-pub mod project;
-pub mod render;
-pub mod util;
-pub mod window;
-pub mod world;
