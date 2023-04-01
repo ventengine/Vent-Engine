4e6c12523c420b900a5a37dca6031461e3bece73
Snowiiii <meieralexander65@gmail.com>
2023-04-01 11:59:42 +0200

    Remove: Old Hard Coded Vertices & Indices

--- crates/vent-runtime/src/render/app_renderer.rs
+++ crates/vent-runtime/src/render/app_renderer.rs
@@ -63,59 +63,6 @@ impl VentApplicationManager {
     }
 }
 
-fn vertex(pos: [i8; 3], tc: [i8; 2]) -> Vertex3D {
-    Vertex3D {
-        _pos: [pos[0] as f32, pos[1] as f32, pos[2] as f32],
-        _tex_coord: [tc[0] as f32, tc[1] as f32],
-    }
-}
-
-fn create_vertices() -> (Vec<Vertex3D>, Vec<u32>) {
-    let vertex_data = [
-        // top (0, 0, 1)
-        vertex([-1, -1, 1], [0, 0]),
-        vertex([1, -1, 1], [1, 0]),
-        vertex([1, 1, 1], [1, 1]),
-        vertex([-1, 1, 1], [0, 1]),
-        // bottom (0, 0, -1)
-        vertex([-1, 1, -1], [1, 0]),
-        vertex([1, 1, -1], [0, 0]),
-        vertex([1, -1, -1], [0, 1]),
-        vertex([-1, -1, -1], [1, 1]),
-        // right (1, 0, 0)
-        vertex([1, -1, -1], [0, 0]),
-        vertex([1, 1, -1], [1, 0]),
-        vertex([1, 1, 1], [1, 1]),
-        vertex([1, -1, 1], [0, 1]),
-        // left (-1, 0, 0)
-        vertex([-1, -1, 1], [1, 0]),
-        vertex([-1, 1, 1], [0, 0]),
-        vertex([-1, 1, -1], [0, 1]),
-        vertex([-1, -1, -1], [1, 1]),
-        // front (0, 1, 0)
-        vertex([1, 1, -1], [1, 0]),
-        vertex([-1, 1, -1], [0, 0]),
-        vertex([-1, 1, 1], [0, 1]),
-        vertex([1, 1, 1], [1, 1]),
-        // back (0, -1, 0)
-        vertex([1, -1, 1], [0, 0]),
-        vertex([-1, -1, 1], [1, 0]),
-        vertex([-1, -1, -1], [1, 1]),
-        vertex([1, -1, -1], [0, 1]),
-    ];
-
-    let index_data: &[u32] = &[
-        0, 1, 2, 2, 3, 0, // top
-        4, 5, 6, 6, 7, 4, // bottom
-        8, 9, 10, 10, 11, 8, // right
-        12, 13, 14, 14, 15, 12, // left
-        16, 17, 18, 18, 19, 16, // front
-        20, 21, 22, 22, 23, 20, // back
-    ];
-
-    (vertex_data.to_vec(), index_data.to_vec())
-}
-
 fn create_texels(size: usize) -> Vec<u8> {
     (0..size * size)
         .map(|id| {
@@ -399,12 +346,11 @@ impl MultiDimensionRenderer for Renderer3D {
         // -------------- DEMO -------------------
         let mut world = World::default();
 
-        //  let (vertex_data, index_data) = create_vertices();
-        let sponza_model = concat!(
+        let model = concat!(
             env!("CARGO_MANIFEST_DIR"),
             "/assets/models/test/Bunny/stanford-bunny.obj"
         );
-        let mut mesh = Mesh3D::new(device, sponza_model);
+        let mut mesh = Mesh3D::new(device, model);
 
         mesh.rotation.x += 150.0;
         mesh.rotation.z += 150.0;
