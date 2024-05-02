f7233c25fa040cbbfc7e3f9c28a382b2b909d5da
Alexander Medvedev <71594357+Snowiiii@users.noreply.github.com>
2024-05-02 17:32:26 +0200

    Made it possible to build on Android
    very early

--- crates/vent-runtime/src/main.rs
+++ crates/vent-runtime/src/main.rs
@@ -3,3 +3,15 @@ use vent_runtime::VentApplication;
 fn main() {
     VentApplication::default();
 }
+
+#[cfg(target_os = "android")]
+use android_activity::{
+    input::{InputEvent, KeyAction, KeyEvent, KeyMapChar, MotionAction},
+    AndroidApp, InputStatus, MainEvent, PollEvent,
+};
+
+#[cfg(target_os = "android")]
+#[no_mangle]
+fn android_main(app: AndroidApp) {
+    VentApplication::default();
+}
